f5ec701abe41c2262e0586a83e62e8b849a0e1a0
marcosranes <marcosranes@gmail.com>
2024-11-22 00:24:44 -0300

    close #7: add card services

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ import logo from "@/public/images/logo4.gif";
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex" aria-label="Inventive4it">
-      <Image src={logo} alt="Inventive4it Logo" width={225} height={0} />
+      <Image src={logo} alt="Inventive4it Logo" width={222} height={66} />
     </Link>
   );
 }
